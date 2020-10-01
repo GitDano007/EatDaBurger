098f7b37f73c79e13bfce1e046b1a8961995e462
Dano Seese <dpseese@yahoo.com>
2020-09-30 20:48:49 -0700

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Dano Burger", false), ("Dano Vegie Burger", false), ("Dano Cheese-Burger", false);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+$(() => {
+    $(".change-state").on("click", function(event) {
+        var id = $(this).data("id");
+        console.log(this)
+        console.log("THIS IS FOR TESTING: " , id)
+        // var nowDevour = $(this).data("nowDevour");
+
+        var nowDevourState = {
+            devoured: 1
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: nowDevourState
+        }).then(() => {
+            // console.log(nowDevour);
+            console.log("This burger is now devoured.");
+            location.reload();
+        });
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+        console.log("this is a thing")
+        var newBurger = {
+            burger_name: $("#newFood").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type:"POST",
+            data: newBurger
+        }).then(() => {
+            console.log("New burger is up.")
+            location.reload();
+        });
+    });
+});
