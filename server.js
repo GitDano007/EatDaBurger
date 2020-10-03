52c7f4353169b6915e90a42c0c38bccd8a73bac7
Dano Seese <dpseese@yahoo.com>
2020-10-03 09:22:37 -0700

    updates

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 5009;
-
 var app = express();
-
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 var routes = require("./controllers/burgers_controllers.js");
-
 app.use(routes);
-
 app.listen(PORT, () => {
   console.log("Server listening on: http://localhost:" + PORT);
 });
