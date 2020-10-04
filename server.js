db87357f89073989c87773f15b3acb3752310703
Dano Seese <dpseese@yahoo.com>
2020-10-04 09:10:07 -0700

    updates

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+
+
 var express = require("express");
-var PORT = process.env.PORT || 5010;
+var PORT = process.env.PORT || 5019;
 var app = express();
+
+
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
